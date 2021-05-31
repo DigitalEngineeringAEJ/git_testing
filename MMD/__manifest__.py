36a9f9541fee760e1cb28d81004bfe5c211b3c44
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-31 10:28:39 +0000

    [ADD] new Module

---
+++ MMD/__manifest__.py
@@ -0,0 +1,33 @@
+# coding: utf-8 -*-
+
+{
+    'name': 'Multi Media Design',
+    
+    'summary': """Hier könnte eine Zusammenfassung stehen"""",
+    
+    'description': """
+        Hier könnte eine Beschreibung des Moduls stehen:
+        -
+        -
+        -
+    """",
+    
+    'author': 'AEJ Member',
+    
+    'website': 'www.aej.odoo.com',
+    
+    'category': 'Website',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
